ef52d3ea4c3e514bf07d9256b404d6625e76ed01
Laura Sucher <l.sucher@gmx.de>
2022-03-03 13:13:41 +0200

    Fixed the show page for therapists

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,11 +45,21 @@ patientfive.photo.attach(io: file5, filename: 'iamge5.png', content_type: 'image
 # Therapist Seed
 puts 'Creating 5 therapists'
 
-therapist1 = { first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: '123456', is_therapist: true }
-therapist2 = { first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: '123456', is_therapist: true }
-therapist3 = { first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: '123456', is_therapist: true }
-therapist4 = { first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: '123456', is_therapist: true }
-therapist5 = { first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: '123456', is_therapist: true }
+therapist1 = { first_name: "Selma", last_name: "Kshlerin", email: "selma@kshlerin.com", password: '123456',
+               is_therapist: true, area_of_expertise: "Addiction & Recovery", years_of_experience: 25,
+               description: "In psychological counseling and therapy I stand for a trusting and respectful cooperation to clarify your problems and concerns. I offer psychological counseling to develop coherent action steps, a goal- and resource-oriented approach to build a better quality of life." }
+therapist2 = { first_name: "Hiram", last_name: "Bogan", email: "hiram@bogan.com", password: '123456',
+               is_therapist: true, area_of_expertise: "Anxiety", years_of_experience: 17,
+               description: "Every therapy draws its effectiveness to a high degree from a sustainable therapeutic relationship. Therefore, in my work with you, an encounter at eye level, characterized by empathy, respect and mutual appreciation, is particularly important to me. I always try to approach my patients with an authentic and compassionate attitude. In this way, we create an atmosphere together that enables you to feel safe and allows you to open up and confront difficult issues." }
+therapist3 = { first_name: "Brent", last_name: "Langworth", email: "brent@langworth", password: '123456',
+               is_therapist: true, area_of_expertise: "Stress Management", years_of_experience: 5.5,
+               description: "I work according to a cognitive-behavioral therapy concept, which means that I assume that certain thought patterns/attitudes and behavior patterns that have contributed to the development of the psychological complaints and the symptoms have been learned and can also be unlearned."}
+therapist4 = { first_name: "Johnson", last_name: "Labadie", email: "johnson@labadie.com", password: '123456',
+               is_therapist: true, area_of_expertise: "Depression", years_of_experience: 1,
+               description: "Based on my qualifications and experience, my therapeutic work encompasses the entire spectrum of mental illness. A particular focus is on the treatment of anxiety disorders, depression, obsessive-compulsive disorder, and post-traumatic stress disorder, as well as support for grief reactions, coping with illness, and burn-out. I work with an emphasis in behavioral therapy and also use a variety of interventions tailored to individual cases." }
+therapist5 = { first_name: "Kristyn", last_name: "Lindgren", email: "kristyn@lindgren.com", password: '123456',
+               is_therapist: true, area_of_expertise: "Grief and Loss", years_of_experience: 2.5,
+               description: "I work according to the focus of depth psychology-based psychotherapy. The therapeutic approach of depth psychology is based on the assumption that unconscious or misunderstood desires, motives or conflicts are hidden behind psychological disorders. It is also referred to as the depth of time, i.e. it is assumed that influences from childhood and adolescence strongly influence our later life."}
 
 therapistone = User.create!(therapist1)
 therapisttwo = User.create!(therapist2)
