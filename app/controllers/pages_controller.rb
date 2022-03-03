1925dce91e5ea29dac9015e7e802828b77cd390b
Ran Milshtein <ranmilshtein@gmail.com>
2022-03-03 14:24:25 +0200

    dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,10 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    @therapist_bookings = current_user.therapist_bookings
+    @patient_bookings = current_user.patient_bookings
+    @profile = current_user
+  end
 end
