78db87d831f87e10c0cc9ead1bb403fa83bbb168
Laura Sucher <l.sucher@gmx.de>
2022-03-07 15:23:07 +0200

    Implemented Notes input

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,24 @@
 class NotesController < ApplicationController
+  def new
+    @note = Note.new
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def create
+    raise
+    @note = Note.new(note_params)
+    @booking = Booking.find(params[:booking_id])
+    @note.booking = @booking
+    if @note.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:content, :is_private)
+  end
 end
