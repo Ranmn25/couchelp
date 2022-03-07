d88600fb98aaeb8c2537b4d37c9bf8a0e42854dc
Talia Azulay <modeoasis1@gmail.com>
2022-03-07 15:35:53 +0200

    done the video chat we need to work on the buttons

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "4106-146-185-57-50.ngrok.io"
+  config.hosts << "7ee9-146-185-57-50.ngrok.io"
 end
