09909fa415fb128e833851e1f47e50cb4e49e147
Ran Milshtein <ranmilshtein@gmail.com>
2022-03-08 17:54:29 +0200

    done with meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
